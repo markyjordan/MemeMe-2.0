b11cea45ef92e88d21fad94662b5a166a6e22af7
Mark Jordan <markybjordan@gmail.com>
2020-05-04 19:54:03 -0700

    Created the Meme struct.

---
+++ MemeMe 2.0/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  MemeMe 2.0
+//
+//  Created by Marky Jordan on 5/4/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    
+    var topTextField: String!
+    var bottomTextField: String!
+    var originalImage: UIImage!
+    var memedImage: UIImage!
+}
