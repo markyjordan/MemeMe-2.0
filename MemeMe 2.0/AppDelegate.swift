af524d84d078dabe91e76da9b5bf6ad45076a2fc
Mark Jordan <markybjordan@gmail.com>
2020-05-27 22:28:06 -0700

    Added bookmarks in AppDelegate file.

--- MemeMe 2.0/AppDelegate.swift
+++ MemeMe 2.0/AppDelegate.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
+// MARK: - AppDelegate: UIResponder, UIApplicationDelegate
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    // MARK: Properties
+    
+    // MARK: UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
