be7e476fbc4d70bace3e62cb602cde2d20c113c3
Mark Jordan <markybjordan@gmail.com>
2020-05-27 22:29:20 -0700

    Added a memes shared data model in the AppDelegate.

--- MemeMe 2.0/AppDelegate.swift
+++ MemeMe 2.0/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: Properties
     
+    var memes = [Meme]()
+    
     // MARK: UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
