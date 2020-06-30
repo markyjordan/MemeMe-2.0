59bb00954df1026182f67d9862a55a661617dcfd
Mark Jordan <markybjordan@gmail.com>
2020-06-29 17:21:10 -0700

    Added SentMemesTableViewCell and SentMemesCollectionViewCell classes.

---
+++ MemeMe 2.0/View Controllers/SentMemesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SentMemesCollectionViewCell.swift
+//  MemeMe 2.0
+//
+//  Created by Marky Jordan on 6/29/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MemeMe 2.0/View Controllers/SentMemesTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SentMemesTableViewCell.swift
+//  MemeMe 2.0
+//
+//  Created by Marky Jordan on 6/29/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesTableViewCell: UITableViewCell {
+
+
+    
+}
