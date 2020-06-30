de1cd4b7fa7705f88557e4d5c760275b5c2aef01
Mark Jordan <markybjordan@gmail.com>
2020-06-30 12:55:18 -0700

    Implemented flow layout setup.

--- MemeMe 2.0/View Controllers/SentMemesCollectionViewController.swift
+++ MemeMe 2.0/View Controllers/SentMemesCollectionViewController.swift
@@ -23,7 +23,13 @@ class SentMemesCollectionViewController: UICollectionViewController {
         // Register cell classes
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
-        // Do any additional setup after loading the view.
+        // flow layout setup
+        let space: CGFloat = 3.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
     }
 
     // MARK: - UICollectionView Data Source Methods
