08242596d3115963817facc0fbf50cc6bdbe7776
Mark Jordan <markybjordan@gmail.com>
2020-07-13 05:03:26 -0700

    Deleted the label property and created two new ones.
    One label property represents tho top text field and the second label property represents the bottom text field.

--- MemeMe 2.0/View Controllers/SentMemesTableViewCell.swift
+++ MemeMe 2.0/View Controllers/SentMemesTableViewCell.swift
@@ -11,5 +11,6 @@ import UIKit
 class SentMemesTableViewCell: UITableViewCell {
 
     @IBOutlet weak var cellImageView: UIImageView!
-    @IBOutlet weak var cellLabel: UILabel!
+    @IBOutlet weak var cellTopTextLabel: UILabel!
+    @IBOutlet weak var cellBottomTextLabel: UILabel!
 }
