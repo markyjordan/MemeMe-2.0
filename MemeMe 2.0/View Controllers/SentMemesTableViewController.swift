47397f48de7b80abab3cb6e8c1eb7c5dba4534a9
Mark Jordan <markybjordan@gmail.com>
2020-07-13 04:31:17 -0700

    Added function that sets empty view upon absense of meme data.
    The empty view contains two label objects that tell the user to tap the 'add' bar button item to create a meme. Additionally, the function sets the left bar button item to nil in the event no meme data exists.

--- MemeMe 2.0/View Controllers/SentMemesTableViewController.swift
+++ MemeMe 2.0/View Controllers/SentMemesTableViewController.swift
@@ -36,15 +36,62 @@ class SentMemesTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
+        // set the placeholder view if there is no meme data to show
+        if appDelegate.memes.count == 0 {
+            setEmptyView(title: "No Memes Stored", message: "Tap '+' to create a new meme!")
+        } else {
+            restoreTableView()
+        }
         return appDelegate.memes.count
+    }
+    
+    // displays an empty placeholder view
+    func setEmptyView(title: String, message: String) {
+        
+        let emptyView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: tableView.frame.height))
+        let titleLabel = UILabel()
+        let messageLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = .black
+        titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 24)
+        messageLabel.textColor = .darkGray
+        messageLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 10)
+        emptyView.addSubview(titleLabel)
+        emptyView.addSubview(messageLabel)
+        
+        // set constraints for empty view
+        titleLabel.centerYAnchor.constraint(equalTo: emptyView.centerYAnchor).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+        messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 3).isActive = true
+        messageLabel.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 10).isActive = true
+        messageLabel.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -10).isActive = true
+        titleLabel.text = title
+        messageLabel.text = message
+        titleLabel.numberOfLines = 0
+        messageLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+        messageLabel.textAlignment = .center
+ 
+        tableView.backgroundView = emptyView
+        tableView.separatorStyle = .none
+        navigationItem.leftBarButtonItem = nil
+    }
+    
+    // restores table view if meme data exists
+    func restoreTableView() {
+        
+        tableView.backgroundView = nil
+        tableView.separatorStyle = .singleLine
+        navigationItem.leftBarButtonItem = editButtonItem
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // dequeue a reusable cell and get the meme object at the specified index path
         let cell = tableView.dequeueReusableCell(withIdentifier: "SentMemesTableViewCell", for: indexPath) as! SentMemesTableViewCell
-        let meme: Meme = self.appDelegate.memes[(indexPath as NSIndexPath).row]
+        let meme: Meme = appDelegate.memes[(indexPath as NSIndexPath).row]
         
         // set the image and label
         cell.imageView?.image = meme.memedImage
@@ -74,7 +121,7 @@ class SentMemesTableViewController: UITableViewController {
         let detailController = self.storyboard!.instantiateViewController(withIdentifier: "MemeDetailViewController") as! MemeDetailViewController
         
         // populate the view controller with data from the selected item
-        detailController.memeToPresent = self.appDelegate.memes[(indexPath as NSIndexPath).row]
+        detailController.memeToPresent = appDelegate.memes[(indexPath as NSIndexPath).row]
         
         // present the view controller using navigation
         self.navigationController!.pushViewController(detailController, animated: true)
